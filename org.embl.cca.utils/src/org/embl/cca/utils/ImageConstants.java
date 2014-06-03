5c4ccda1afd2161c3006762af9901d65a34e004c
Gábor Náray <gnaray@embl.fr>
2014-06-03 16:30:51 +0200

    Image list changed a bit.

--- org.embl.cca.utils/src/org/embl/cca/utils/ImageConstants.java
+++ org.embl.cca.utils/src/org/embl/cca/utils/ImageConstants.java
@@ -8,12 +8,15 @@ public class ImageConstants {
 	public static final String IMG_RUN = "icons/run.gif";
 	public static final String IMG_COPY = "icons/copy.gif";
 	public static final String IMG_ALPHA_MODE_FOLDER = "icons/alpha_mode_folder.png";
-	public static final String IMG_ALPHA_MODE = "icons/alpha_mode.png";
-	public static final String IMG_SAVE_AS = "icons/document-save-as.png";
-	public static final String IMG_CANCEL = "icons/gtk-cancel.png";
+	public static final String IMG_ALPHA_MODE = "icons/alpha_mode.gif";
+	public static final String IMG_REFRESH = "icons/refresh.gif";
+	public static final String IMG_CONVERT = "icons/convert.gif";
+	public static final String IMG_COLLAPSE_ALL = "icons/CollapseAll.png";
+	public static final String IMG_CLONE_EL = "icons/clone-el.gif";
 
 	public static String[] IMAGES = new String[] { IMG_FOLDER, IMG_FILE,
 		IMG_CLOSE, IMG_RUN, IMG_COPY,
 		IMG_ALPHA_MODE_FOLDER, IMG_ALPHA_MODE,
-		IMG_SAVE_AS, IMG_CANCEL };
+		IMG_REFRESH, IMG_CONVERT, IMG_COLLAPSE_ALL, IMG_CLONE_EL
+	};
 }
