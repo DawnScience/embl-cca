b029750e1ef7d8aada14691f3b367c15416998df
Peter Chang <Peter.Chang@diamond.ac.uk>
2014-06-26 16:05:35 +0100

    Updated call from arange to createRange

--- org.embl.cca.dviewer/src/org/embl/cca/dviewer/plotting/tools/DiffractionLineProfileTool.java
+++ org.embl.cca.dviewer/src/org/embl/cca/dviewer/plotting/tools/DiffractionLineProfileTool.java
@@ -306,7 +306,7 @@ public class DiffractionLineProfileTool extends ProfileTool {
 		
 		final AbstractDataset intensity = profileData[0];
 		intensity.setName(region.getName());
-		final AbstractDataset indices = IntegerDataset.arange(0, intensity.getSize(), 1d);
+		final AbstractDataset indices = IntegerDataset.createRange(0, intensity.getSize(), 1d);
 		indices.setName("Pixel");
 		
 		final ILineTrace trace = (ILineTrace)profilePlottingSystem.getTrace(region.getName());
