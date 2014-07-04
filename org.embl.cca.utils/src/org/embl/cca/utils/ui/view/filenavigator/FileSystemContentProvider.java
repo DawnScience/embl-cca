0cbbb77bfbe9215478d50a7a5f1328db9eab0c02
Gábor Náray <gnaray@embl.fr>
2014-07-04 16:11:28 +0200

    Added copyright headers.

--- org.embl.cca.utils/src/org/embl/cca/utils/ui/view/filenavigator/FileSystemContentProvider.java
+++ org.embl.cca.utils/src/org/embl/cca/utils/ui/view/filenavigator/FileSystemContentProvider.java
@@ -1,17 +1,27 @@
+/*
+ * Copyright 2014 Diamond Light Source Ltd. and EMBL
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.embl.cca.utils.ui.view.filenavigator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.runtime.Assert;
-import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.swt.widgets.TreeItem;
 import org.embl.cca.utils.datahandling.EFile;
-import org.embl.cca.utils.datahandling.collection.ListUtils;
 import org.embl.cca.utils.datahandling.text.StringUtils;
 import org.embl.cca.utils.ui.widget.support.treeviewer.TreeContentProvider;
 import org.embl.cca.utils.ui.widget.support.treeviewer.TreeNode;
-import org.embl.cca.utils.ui.widget.support.treeviewer.TreeNode.TreeNodeState;
 
 public class FileSystemContentProvider extends TreeContentProvider {
 	public static final String MSG_NULL_FILE = "The file is null, it must not be null.";
