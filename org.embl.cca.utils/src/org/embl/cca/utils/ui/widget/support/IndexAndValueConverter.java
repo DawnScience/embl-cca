c3da7b2a82a88d5edf816eab4db2ea128bcc1781
Gábor Náray <gnaray@embl.fr>
2014-11-28 17:25:13 +0000

    Added final to arguments.

--- org.embl.cca.utils/src/org/embl/cca/utils/ui/widget/support/IndexAndValueConverter.java
+++ org.embl.cca.utils/src/org/embl/cca/utils/ui/widget/support/IndexAndValueConverter.java
@@ -1,6 +1,6 @@
 package org.embl.cca.utils.ui.widget.support;
 
 public abstract class IndexAndValueConverter {
-	public abstract Number index2Value(int i);
-	public abstract int value2index(Number real);
+	public abstract Number index2Value(final int i);
+	public abstract int value2index(final Number real);
 }
