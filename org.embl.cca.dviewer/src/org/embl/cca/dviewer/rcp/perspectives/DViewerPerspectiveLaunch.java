45fd775e054395361ab42182263803028bab8f46
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-05-12 11:38:52 +0100

    simplify code

--- org.embl.cca.dviewer/src/org/embl/cca/dviewer/rcp/perspectives/DViewerPerspectiveLaunch.java
+++ org.embl.cca.dviewer/src/org/embl/cca/dviewer/rcp/perspectives/DViewerPerspectiveLaunch.java
@@ -1,11 +1,6 @@
 package org.embl.cca.dviewer.rcp.perspectives;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
+import org.dawb.common.ui.perspective.AbstractPerspectiveLaunch;
 
 /**
  * Data Browsing Perspective launcher
@@ -13,33 +8,10 @@ import org.eclipse.ui.WorkbenchException;
  * @author Gábor Náray
  *
  **/
-public class DViewerPerspectiveLaunch implements IWorkbenchWindowActionDelegate {
+public class DViewerPerspectiveLaunch extends AbstractPerspectiveLaunch {
 
 	@Override
-	public void run(final IAction action) {
-		try {
-			PlatformUI.getWorkbench().showPerspective(DViewerPerspective.PERSPECTIVE_ID,PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-		} catch (final WorkbenchException e) {
-			e.printStackTrace();
-		}
+	public String getID() {
+		return DViewerPerspective.PERSPECTIVE_ID;
 	}
-
-	@Override
-	public void selectionChanged(final IAction action, final ISelection selection) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void init(final IWorkbenchWindow window) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
